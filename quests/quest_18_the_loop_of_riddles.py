1ad1aefcd22f8905d8aeb2abddd548ccf7e239c1
Orion Seruvumba <o.seruvumba@alustudent.com>
2026-02-24 14:24:52 +0200

    updating quest

---
+++ quests/quest_18_the_loop_of_riddles.py
@@ -0,0 +1,13 @@
+secret_number = 7
+guess = 0 
+
+print ("Welcome to the Guessing Game!")
+print ("Try to guess my secret number between 1 and 10.")
+while guess != secret_number:
+    guess = int(input("Enter your guess: "))
+    if guess < secret_number:
+        print ("Too low, try again. ")
+    elif guess >secret_number:
+        print ("Too high, try again. ")
+    else:
+        print ("Congratulations! It's correct!")
