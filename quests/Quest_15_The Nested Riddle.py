1b4e47e2db7bf04cb826355f4e322ec7f9bcbf40
Morel121 <n.morel@alustudent.com>
2026-02-24 13:56:45 +0200

    Fifteen

--- quests/Quest_15_The Nested Riddle.py
+++ quests/Quest_15_The Nested Riddle.py
@@ -1,6 +1,6 @@
-direction = input("Do you want to go left or right? ")
+direction = input("Do you want to go left or right?")
 if direction == "left":
-    action = input("Do you 'swim' or 'wait'? ")
+    action = input("Do you 'swim' or 'wait'?")
     if action == "swim":
         print("You find a treasure!")
     else:
