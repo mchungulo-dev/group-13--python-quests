1093ca956dcad2e0997600089f1ae9a5f4d19d6c
shaun <s.muhizi@alustudent.com>
2026-02-24 12:25:33 +0200

    updating quests

---
+++ quests/quest_12_the_two_path_cave.py
@@ -0,0 +1,5 @@
+password =input("Enter the password:")
+if password == "secret123":
+    print("Access Granted")
+else:
+    print("Access Denied")
---
+++ quests/quest_14_the_logical_gatekeeper.py
@@ -0,0 +1,9 @@
+age = int(input("How old are you?"))
+gold = int(input("How many gold coins do you have?")) 
+
+#Both condittions must be trure for the whole statement to be true
+if age >=18 and gold >=20:
+    print("welcome to the club")
+else:
+    print("you  can't enter the club")
+    
