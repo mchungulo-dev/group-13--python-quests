295c7e50a3c30115d70725471ddf9806e50dc845
Morel121 <n.morel@alustudent.com>
2026-02-24 13:52:42 +0200

    updating quest

--- quests/quest_17_the_endless_staircase.py
+++ quests/quest_17_the_endless_staircase.py
@@ -2,5 +2,5 @@ counter = 0
 while counter < 5:
     print(counter)
     counter += 1
-    print("stop! The counter reached 5.")
-bjjhb
+    if counter == 5:
+        print("Stop! The counter reached 5!")
